f0eceecd3e77fd226367b1981b9323fb253ccf7b
Julien Guanzon <julienguanzon@gmail.com>
2015-02-19 13:55:41 -0700

    Initial Commit

---
+++ TimeTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeTracker
+//
+//  Created by Julien Guanzon on 2/19/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeTracker
+//
+//  Created by Julien Guanzon on 2/19/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
